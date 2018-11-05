a512033786ece24542ff3dcaa7952139b7af6d94
Pericles Costa <pericles.costa@tecnotechs.org>
2018-11-05 18:24:59 -0300

    #2 - Manipulacao dos graficos - PARTE 1

--- static/js/execution/admin-script.js
+++ static/js/execution/admin-script.js
@@ -4,6 +4,7 @@ if (!$) {
 }
 
 var interval = null;
+var last_length = 0;
 
 function runtime_loop(execute_runtime) {
     if (execute_runtime) {
@@ -17,73 +18,86 @@ function load_checkpoints() {
     }).done(function(result){
         var rows = '';
         var row = '';
-        var labels = [];
-        var data = [];
+        var labels = ['Success', 'Fail'];
+        var data = {
+            'module': {
+                'labels': labels,
+                'content': [0, 0],
+            },
+            'script': {
+                'labels': labels,
+                'content': [0, 0],
+            },
+        };
 
-        $(result.checkpoints).each(function(){
-            row = $(this)[0];
-            rows += '<tr>';
-                rows += '<td>'+ row.reference_display +'</td>';
-                rows += '<td>'+ row.object_display +'</td>';
-                rows += '<td class="'+ (row.status == 1 ? 'td-success' : 'td-fail') +'">'+ row.status_display +'</td>';
-                rows += '<td>'+ row.name +'</td>';
-                rows += '<td>'+ (row.description == null ? '-' : row.description) +'</td>';
-                rows += '<td>'+ row.date_checkpoint +'</td>';
-            rows += '</tr>';
+        if (result.checkpoints.length > last_length) {
+            $(result.checkpoints).each(function(){
+                row = $(this)[0];
+                rows += '<tr>';
+                    rows += '<td>'+ row.reference_display +'</td>';
+                    rows += '<td>'+ row.object_display +'</td>';
+                    rows += '<td class="'+ (row.status == 1 ? 'td-success' : 'td-fail') +'">'+ row.status_display +'</td>';
+                    rows += '<td>'+ row.name +'</td>';
+                    rows += '<td>'+ (row.description == null ? '-' : row.description) +'</td>';
+                    rows += '<td>'+ row.date_checkpoint +'</td>';
+                rows += '</tr>';
 
-            labels.push(row.name);
-            data.push(row.reference);
-        });
+                switch(row.object) {
+                    case 1:
+                        // data['module']['labels'].push(row.name);
+                        data['module']['content'][row.status-1]++;
+                        break;
+                    case 2:
+                        // data['script']['labels'].push(row.name);
+                        data['script']['content'][row.status-1]++;
+                }
+            });
+
+            last_length = result.checkpoints.length;
+            $('#date-started').html(result.date_started);
+        } else {
+            return false;
+        }
 
         $('#checkpoints-table tbody').html(rows);
-        render_chart(labels, data);
+
+        render_chart(data);
 
         if (row.reference == 2) {
             clearInterval(interval);
-            window.location.reload();
+            $('#date-finished').html(result.date_finished);
         }
     });
 }
 
-function render_chart(labels, data, type='line'){
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: type,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: '# of Votes',
-                data: data,
-                // backgroundColor: [
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(75, 192, 192, 0.2)',
-                //     'rgba(153, 102, 255, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)'
-                // ],
-                // borderColor: [
-                //     'rgba(255,99,132,1)',
-                //     'rgba(54, 162, 235, 1)',
-                //     'rgba(255, 206, 86, 1)',
-                //     'rgba(75, 192, 192, 1)',
-                //     'rgba(153, 102, 255, 1)',
-                //     'rgba(255, 159, 64, 1)'
-                // ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
+function render_chart(data, type='pie'){
+    for(var key in data) {
+        var ctx = document.getElementById(key+'-chart');
+        var myChart = new Chart(ctx, {
+            type: type,
+            data: {
+                labels: data[key]['labels'],
+                datasets: [{
+                    data: data[key]['content'],
+                    backgroundColor: [
+                        '#12c56e',
+                        '#c51212',
+                    ],
+                    borderWidth: 1
                 }]
             },
-            animation: {
-                duration: 0
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                },
+                // animation: {
+                //     duration: 0
+                // }
             }
-        }
-    });
+        });
+    }
 }
