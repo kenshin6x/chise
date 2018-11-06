312eca6fd720a15f845f740ae50daf5cef7e0a67
Junior Andrade <seisxis@gmail.com>
2018-11-06 16:43:43 -0200

    report

--- chise/execution/reports/execution.py
+++ chise/execution/reports/execution.py
@@ -1,18 +1,27 @@
+from chise import settings
+from django.utils.translation import ugettext_lazy as _
 from io import BytesIO
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter, A4
-from reportlab.lib.units import inch
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4, letter, landscape
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.units import inch, mm
 
 class ExecutionReport:
     object = None
     buffer = None
-    c = None
+    r = None
 
     def __init__(self, object, *args, **kwargs):
         self.object = object
 
         self.buffer = BytesIO()
-        self.c = canvas.Canvas(self.buffer)
+        self.r = SimpleDocTemplate(self.buffer, 
+                                pagesize=landscape(A4), 
+                                rightMargin=30,
+                                leftMargin=30, 
+                                topMargin=30,
+                                bottomMargin=30)
         self.draw()
 
     def pdf(self):
@@ -22,7 +31,37 @@ class ExecutionReport:
         return pdf
         
     def draw(self):
-        self.c.drawString(100,750, "Welcanvasome to Reportlab!")
-        
-        self.c.showPage()
-        self.c.save()
+        elements = []
+
+        # checkpoints
+        data = [(
+            _('Status'),
+            _('Reference'),
+            _('Object'),
+            _('Name'),
+            _('Description'),
+            _('Date Checkpoint')
+        ),]
+
+        for object in self.object.checkpoints.all():
+            data.append((
+                object.get_status_display(),
+                object.get_reference_display(),
+                object.get_object_display(),
+                object.name,
+                object.description,
+                object.date_checkpoint,
+            ))
+ 
+        table = Table(data, colWidths=45*mm)
+        table.setStyle(TableStyle([
+            ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+            ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+            ('VALIGN', (0, 0), (-1, 0), 'MIDDLE'),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.gray)
+        ]))
+    
+
+        elements.append(table)
+
+        self.r.build(elements)
